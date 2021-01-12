1d5eb04f753462524b76591a29bc1d65bb2c64f1
lixue <62551662+lixue395@users.noreply.github.com>
2021-01-12 16:24:32 +0800

    登录页面

--- wj-vue/src/components/Login.vue
+++ wj-vue/src/components/Login.vue
@@ -1,5 +1,7 @@
 <template>
   <body id="paper">
+    <!--登录页面-->
+    <!--写一个body样式，是为了更换背景图，覆盖掉浏览器（用户代理）的默认样式。-->
     <el-form :model="loginForm" :rules="rules" class="login-container" label-position="left"
              label-width="0px" v-loading="loading">
       <h3 class="login_title">系统登录</h3>
@@ -36,6 +38,9 @@
         loading: false
       }
     },
+    /* methods定义了登录按钮的点击方法，即向后端 /login 接口发送数据，获得成功的响应后，页面跳转到 /index
+    因为之前设置了默认的 URL，所以请求实际上发到了 http://localhost:8443/api/login
+    */
     methods: {
       login () {
         var _this = this
